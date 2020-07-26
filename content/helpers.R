ad5548d5ff5a3733fffecac4be5203b6a5f52d3d
Danielle Navarro <d.navarro@unsw.edu.au>
2020-07-26 21:20:18 +1000

    first pass at front page

---
+++ content/helpers.R
@@ -0,0 +1,147 @@
+make_gallery <- function(id, name, dir, text) {
+
+  images <- list.files(here::here("img", dir), pattern = "png$")
+  links <- paste0('<a href = "img/', dir, "/" ,images, '"><img src = "sml/', dir, "/", images, '"></a>')
+
+  paste(
+    '<!-- Start gallery Area -->',
+    paste0('<section class="gallery-area section-gap" id="', id, '">'),
+    '<div class="container">',
+    '<div class="row justify-content-center">',
+    '<div class="col-md-8 pb-30 header-text">',
+    paste0('<h1 class="text-white">', name, '</h1>'),
+    '<p>',
+    text,
+    '</p>',
+    '</div>',
+    '</div>',
+    '<div class="gal">',
+    paste(links, collapse = "\n"),
+    '</div>',
+    '</div>',
+    '</section>',
+    sep = "\n"
+  )
+}
+
+
+make_menu <- function() {
+
+  paste(
+    '    <!-- Start Header Area -->',
+    '      <header class="default-header">',
+    '      <div class="container">',
+    '      <div class="header-wrap">',
+    '      <div class="header-top d-flex justify-content-between align-items-center">',
+    '      <div class="logo">',
+    '      <a href="#home"><img src="img/logo.png" alt=""></a>',
+    '      </div>',
+    '      <div class="main-menubar d-flex align-items-center">',
+    '      <nav class="hide">',
+    '      <a href="#home">Home</a>',
+    '      <a href="#quarantine">Quarantine</a>',
+    '      <a href="#voronoise">Voronoise</a>',
+    '      <a href="#flametree">Flametree</a>',
+    '      <a href="#scrawl">Scrawl</a>',
+    '      <a href="#scope">Scope</a>',
+#    '      <a href="#popart">Pop Art</a>',
+#    '      <a href="#dust">Dust</a>',
+#    '      <a href="#nye2019">NYE 2019</a>',
+#    '      <a href="#rosemary">Rosemary</a>',
+    '      </nav>',
+    '      <div class="menu-bar"><span class="lnr lnr-menu"></span></div>',
+    '      </div>',
+    '      </div>',
+    '      </div>',
+    '      </div>',
+    '      </header>',
+    '      <!-- End Header Area -->',
+    sep = "\n"
+  )
+
+}
+
+
+
+make_carousel <- function() {
+
+  paste(
+
+    '    <!-- start banner Area -->',
+    '      <section class="banner-area relative" id="home">',
+    '      <div class="slider"><div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">',
+    '      ',
+    '      <div class="carousel-inner" role="listbox">',
+    '      <div class="carousel-item active" style="background-image: url(\'img/scopes/scope_03.png\')"></div>',
+    '      <div class="carousel-item" style="background-image: url(\'img/flametree/007_ft_298d.png\')"></div>',
+    '      <div class="carousel-item" style="background-image: url(\'img/flametree/001_flametree_20_13.png\')"></div>',
+    '      </div>',
+    '      ',
+    '      <div class="carousel-caption d-md-block">',
+    '      <p style="font-size:150%">art by</p>',
+    '      <h2 style="font-size:200%" class="text-uppercase">Danielle Navarro</h2>',
+    '      </div>',
+    '      ',
+    '      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">',
+    '      <span class="carousel-control-prev-icon" aria-hidden="true"></span>',
+    '      <span class="sr-only">Previous</span>',
+    '      </a>',
+    '      ',
+    '      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">',
+    '      <span class="carousel-control-next-icon" aria-hidden="true"></span>',
+    '      <span class="sr-only">Next</span>',
+    '      </a>',
+    '      </div></div>',
+    '      </section>',
+    '      <!-- End banner Area -->',
+    sep = "\n"
+  )
+}
+
+
+make_profile <- function() {
+  paste(
+
+    '    <!-- Start About Area -->',
+    '      <section class="gallery-area section-gap" id="about">',
+    '      <div class="container">',
+    '      <div class="row d-flex align-items-center">',
+    '      <div class="col-lg-3 about-left">',
+    '      <img class="img-fluid" src="img/about-img-smol.jpg" alt="">',
+    '      </div>',
+    '      <div class="col-lg-9 about-right">',
+    '      <p style="color: #666666">',
+    '      <span aria-hidden="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum molestie est sed lacinia. Curabitur quis sodales augue. Integer ultrices nunc tortor, non lacinia arcu fermentum faucibus. Cras odio dolor, pellentesque at </span><span class="text-white">generative art with R</span><span aria-hidden="true"> vehicula est. Nulla in finibus est. In hac habitasse platea dictumst. Vivamus nec sem sit amet ante efficitur elementum a vitae sapien. Integer sed ante tincidunt, rutrum enim at, imperdiet turpis. In imperdiet faucibus neque a tincidunt. Sed mollis consequat lorem. Phasellus consectetur, nisl in sagittis vehicula, est nisl eleifend </span> <a href="https://djnavarro.net">djnavarro.net</a> <span aria-hidden="true">arcu, eget pellentesque nunc sem non mi. Quisque quis scelerisque dolor. Donec scelerisque purus quis ex condimentum blandit. Vivamus non lorem eros. Aliquam ac dui at ante vehicula cursus nec et lectus. Integer et </span> <a href="https://twitter.com/djnavarro">twitter.com/djnavarro</a> <span aria-hidden="true">mauris posuere, faucibus orci vel, facilisis urna. Vivamus vitae porta lorem, eleifend imperdiet risus. Aliquam sit amet justo id turpis euismod ultrices. </span> <a href="https://github.com/djnavarro">github.com/djnavarro</a> <span aria-hidden="true"> Duis id dui velit. Nullam non purus ut tortor blandit faucibus sit amet non lacus. Sed commodo pulvinar ipsum in tristique. Maecenas finibus, enim vel sagittis faucibus, lectus lectus varius neque, eu cursus felis diam in quam.</span>',
+    '    </p>',
+    '      </div>',
+    '      </div>',
+    '      </div>	',
+    '      </section>',
+    '      <!-- End About Area -->',
+    sep = "\n"
+
+  )
+}
+
+
+make_footer <- function() {
+
+  paste(
+    '<!-- start footer Area -->',
+    '<footer class="footer-area">',
+    '<div class="container">',
+    '<div class="row footer-bottom d-flex justify-content-between">',
+    '<p class="col-lg-8 col-sm-12 footer-text m-0 text-white">artwork by <a href="https://djnavarro.net">danielle navarro</a>, licenced as <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA 4.0</a>, site template by <a href="https://colorlib.com/wp/themes/philosophy/">colorlib</a></p>',
+    '<div class="col-lg-4 col-sm-12 footer-social">',
+    '<a href="https://art.djnavarro.net"><i class="fa fa-home"></i></a>',
+    '<a href="https://twitter.com/djnavarro"><i class="fa fa-twitter"></i></a>',
+    '<a href="mailto:d.navarro@unsw.edu.au"><i class="fa fa-paper-plane"></i></a>',
+    '<a href="https://github.com/djnavarro"><i class="fa fa-github"></i></a>',
+    '</div>',
+    '</div>',
+    '</div>',
+    '</footer>',
+    '<!-- End footer Area -->',
+    sep = "\n"
+  )
+}
